91cb1ad15a2b7c5b00f602ca66556d6db6d6b30f
teacher_cheater <johnnykayfun@gmail.com>
2022-10-28 17:09:29 +0300

    init, add components

---
+++ src/components/action/action.svelte
@@ -0,0 +1,22 @@
+<script>
+  import s from "./action.module.css";
+</script>
+
+<section class={s.action}>
+  <div class={s.content}>
+    <div class={s.left}>
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img class={s.image} src="img/img-1.png" alt="image" />
+    </div>
+    <div class={s.right}>
+      <h2 class={s.text}>
+        Пусть ваш ребёнок познает мир, а о пятнах на его одежде позаботится
+        Meine Liebe. Участвуйте в конкурсе и выигрывайте призы!
+      </h2>
+      <a href="#" class={s.button}>Участвовать</a>
+    </div>
+  </div>
+</section>
+
+<style>
+</style>
---
+++ src/components/header/header.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Action from "../action/action.svelte";
+  import s from "./header.module.css";
+</script>
+
+<header class={s.header}>
+  <h1 class={s.title}>1000 пятен<span /></h1>
+  <p class={s.text}>1000 классных моментов</p>
+  <Action />
+</header>
+
+<style>
+</style>
